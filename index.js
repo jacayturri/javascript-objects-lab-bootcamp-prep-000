6c396c9d177b464f13b51a10727d77e907650fc9
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-31 21:08:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]:value});
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign({}, recipes, {[key]:value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
